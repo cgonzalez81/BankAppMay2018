ec4a0815c5bedddc63dc1abafda1abfe83634c7b
Carmen Gonzalez <carmen.gonzalez81@yahoo.com>
2018-06-04 23:43:49 -0700

    Methods and properties

---
+++ BankApp/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+	#region Properties
+	/// <summary>
+	/// Bank account
+	/// </summary>
+	class Account
+	{
+		/// <summary>
+		/// unique account number
+		/// </summary>
+		public int AccountNumber { get; set; }
+		/// <summary>
+		/// email address of the user on the account
+		/// </summary>
+		public string EmailAddress { get; set; }
+		public decimal Balance { get; set; }
+		public string TypeOfAccount { get; set; }
+		#endregion
+
+	}
+}
