48be12298ee945c9f55dd6490a2bd6a591c56be3
Carmen Gonzalez <carmen.gonzalez81@yahoo.com>
2018-06-06 23:54:41 -0700

    Methods and properties, second try

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,23 +6,67 @@ using System.Threading.Tasks;
 
 namespace BankApp
 {
-	#region Properties
 	/// <summary>
 	/// Bank account
 	/// </summary>
 	class Account
 	{
+		private static int lastAccountNumber = 0;
+
+		#region Properties
 		/// <summary>
 		/// unique account number
 		/// </summary>
-		public int AccountNumber { get; set; }
+		public int AccountNumber { get; private set; }
 		/// <summary>
 		/// email address of the user on the account
 		/// </summary>
 		public string EmailAddress { get; set; }
-		public decimal Balance { get; set; }
+		//make balance private so user cant determine it
+		public decimal Balance { get; private set; }
 		public string TypeOfAccount { get; set; }
+		public DateTime CreatedDate {get; set; }
 		#endregion
+
+		/*public Account()
+		{
+			//lastAccountNumber += 1;
+			//AccountNumber = lastAccountNumber;
+			//ctrl K and Ctrl C for multi line comment, ctrl U to uncomment
+			//make the comment example shorter with next line
+			//lastAccountNumber++; means increment of 1
+			//++lastAccountNumber; aLso means increment of 1
+			AccountNumber = ++lastAccountNumber;
+			CreatedDate = DateTime.UtcNow;
+		}*/
+
+		#region Methods
+
+
+			/// <summary>
+			/// deposit into account
+			/// </summary>
+			/// <param name="amount">explain here what the parameter is about (amount to deposit)</param>
+			/// <returns>Updated Balance goes here</returns>
+	public decimal Deposit(decimal amount)
+	{
+		//=+ means add it to itself(Balance = Balance + amount
+		Balance += amount;
+			return Balance;
+	}
+
+		public void Withdraw(decimal amount)
+		{
+			// this amount is private to this method
+			//with void, no return is expected
+			Balance -= amount;
+
+		}
+
+
+
+		#endregion
+
 
 	}
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -8,8 +8,27 @@ namespace BankApp
 {
 	class Program
 	{
+		//when it has closed parenth. it is a method
+		//only one main method for every project
 		static void Main(string[] args)
 		{
+			//object, instance of a class, instantiation
+			//read from right to left
+			//account is the name for the memory, 
+			//when we name it acct, it knows where to go
+			var account = new Account();
+			//account.AccountNumber = 12345;
+			//customer should only be able to tell u email and type,not acct # & balance
+			account.EmailAddress = "test@test.com";
+			account.TypeOfAccount = "Checking";
+			//account.Balance = 100000000;
+
+			Console.WriteLine($"AN: {account.AccountNumber}, Balance {account.Balance}, EA {account.EmailAddress}");
+
+			var account2 = new Account();
+			account2.EmailAddress = "test2@test.com";
+
+			Console.WriteLine(account2.Balance);
 		}
 	}
 }
